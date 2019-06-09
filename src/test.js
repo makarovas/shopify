4d81d59108e1aec9dc3745c81a55e98be7a4f496
Alexander Makarof <makarofalex@gmail.com>
2019-06-09 20:29:07 +0300

    save

--- src/test.js
+++ src/test.js
@@ -1,16 +1,31 @@
-function pallidrom(string) {
-	string = string.toLowerCase();
-	var charactersArr = string.split('');
-	var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-	var lettersArr = [];
-	charactersArr.forEach(char => {
-		if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
-	})
-	if (lettersArr.join('') === lettersArr.reverse().join()) {
-		return true;
-	} else {
-		return false;
-	}
+const alpha = [];
+
+function genCharArray(charA, charZ) {
+  let i = charA.charCodeAt(0);
+  const j = charZ.charCodeAt(0);
+  for (; i <= j; ++i) {
+    alpha.push(String.fromCharCode(i));
+  }
+  return alpha;
+}
+genCharArray('a', 'z');
+
+
+function caesarCipher(str, num) {
+  let newString = [];
+  const lowerCaseString = str.toLowerCase();
+  for (let i = 0; i < lowerCaseString.length; i++) {
+    const currentLetter = lowerCaseString[i];
+    if (currentLetter === ' ') {
+      newString += currentLetter;
+      continue;
+    }
+    const currentIndex = alpha.indexOf(currentLetter);
+    let newIndex = currentIndex + num;
+    if (newIndex > 25) newIndex -= 26;
+    if (newIndex < 0) newIndex = 26 + newIndex;
+    newString += alphabet[newIndex];
+  }
 }
 
-console.log(pallidrom("race car"));
+caesarCipher('zoo keeper', 2);
